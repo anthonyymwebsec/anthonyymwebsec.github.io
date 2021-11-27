001995aca1c0912262803e7afbe944cce34604b1
anthonyym <ym.hades@gmail.com>
2021-11-26 19:52:02 -0500

    fix get

--- scripts.js
+++ scripts.js
@@ -35,8 +35,9 @@ rtdb.get(chatroomNamesRef).then((snapshot) => {
   if (snapshot.exists()) {
     // chatroomNamesAll: key/value with random_id: {name: "room1", users: {uid: {uid: "uid"}}}
     let chatroomNamesAll = snapshot.val();
-    for (let i = 0; i < Object.keys(chatroomNamesAll).length; i++) {
-      if (!!chatroomNamesAll[i].users[currentUser.uid]) {
+    let keys = Object.keys(chatroomNamesAll);
+    for (let i = 0; i < keys.length; i++) {
+      if (!!chatroomNamesAll[keys[i]].users[currentUser.uid]) {
         chatroomNames.push(chatroomNamesAll[i]);
       }
     }
