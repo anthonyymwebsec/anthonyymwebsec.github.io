c457d2fe78774b0dcb32031acfc41dab0696f1cc
anthonyym <ym.hades@gmail.com>
2021-11-26 20:16:56 -0500

    fix chatroom size

--- scripts.js
+++ scripts.js
@@ -38,7 +38,7 @@ rtdb.get(chatroomNamesRef).then((snapshot) => {
     let keys = Object.keys(chatroomNamesAll);
     for (let i = 0; i < keys.length; i++) {
       if (!!chatroomNamesAll[keys[i]].users[currentUser.uid]) {
-        addChatTab(chatroomNamesAll[keys[i]].name, chatroomNamesAll[keys[i]].users);
+        addChatTab(chatroomNamesAll[keys[i]].name, Object.keys(chatroomNamesAll[keys[i]].users).length);
         // chatroomNames.push(chatroomNamesAll[keys[i]]);
       }
     }
